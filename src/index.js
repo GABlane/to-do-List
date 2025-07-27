9f51e6a9722863fc22a91d0a902e5777ce49d4a7
GABlane <ofiangga.bscs@gmail.com>
2025-07-27 23:22:52 +0800

    planning how to approach

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import {compareAsc, format} from "date-fns";
 import "./styles.css";
+
+const home = document.getElementById("home");
+const important = document.getElementById("important");
+const addProject = document.getElementById("add");
+const addTodo = document.getElementById("add-todo");
+
+const date = new Date();
+
+
+
+home.addEventListener("click", () => {
+
+});
+
+document.getElementById("here").innerHTML = `title ${date} `
